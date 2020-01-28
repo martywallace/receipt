70d2baff2e9fa9966e95aeccd0e2ff59d1a0f741
Marty Wallace <marty@martywallace.com>
2020-01-28 16:56:52 +1100

    WIP property type.

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,68 @@
 import { EOL } from "os";
+import { ReceiptFormattingException } from "./exceptions";
 import {
   ReceiptOptions,
+  ReceiptPropertiesItemOptions,
+  ReceiptPropertiesLine,
   ReceiptRulerItemOptions,
-  ReceiptTextItemOptions
+  ReceiptTextItemOptions,
+  ReceiptItemDataProvider,
+  Align,
+  Layout
 } from "./types";
-import { ReceiptFormattingException } from "./exceptions";
 
 export * from "./types";
 
+export class ReceiptSupport {
+  constructor(protected readonly options: ReceiptOptions) {}
+
+  public toAlignedString(
+    value: string,
+    align: Align,
+    horizontalPadding: number = 0
+  ): string {
+    const padToken: string = " ";
+    const maximumLineLength: number =
+      this.options.width - horizontalPadding * 2;
+
+    while (value.length < maximumLineLength) {
+      if (align === "left") {
+        value = value + padToken;
+      } else if (align === "right") {
+        value = padToken + value;
+      } else {
+        value = padToken + value + padToken;
+      }
+    }
+
+    return value
+      .substr(0, maximumLineLength)
+      .padStart(this.options.width - horizontalPadding)
+      .padEnd(this.options.width);
+  }
+
+  public toCurrencyString(base: number): string {
+    return (base / 100).toLocaleString(this.options.locale, {
+      style: "currency",
+      currency: this.options.currency
+    });
+  }
+}
+
 export class Receipt {
   public readonly options: ReceiptOptions;
+  public readonly support: ReceiptSupport;
   protected items: readonly ReceiptItem<unknown>[] = [];
 
   constructor(options: Partial<ReceiptOptions>) {
     this.options = {
-      width: 60,
+      locale: "en-US",
       currency: "USD",
+      width: 60,
       ...options
     };
+
+    this.support = new ReceiptSupport(this.options);
   }
 
   public add(item: ReceiptItem<unknown>): this {
@@ -25,8 +70,16 @@ export class Receipt {
     return this;
   }
 
-  public text(text: string, options?: ReceiptTextItemOptions): this {
-    return this.add(new ReceiptTextItem(text, options));
+  public text(
+    text: string | ReceiptItemDataProvider<string>,
+    options?: ReceiptTextItemOptions
+  ): this {
+    return this.add(
+      new ReceiptTextItem(
+        text instanceof Function ? text(this.support) : text,
+        options
+      )
+    );
   }
 
   public ruler(options?: ReceiptRulerItemOptions): this {
@@ -35,6 +88,20 @@ export class Receipt {
 
   public break(): this {
     return this.add(new ReceiptBreakItem());
+  }
+
+  public properties(
+    lines:
+      | readonly ReceiptPropertiesLine[]
+      | ReceiptItemDataProvider<readonly ReceiptPropertiesLine[]>,
+    options?: ReceiptPropertiesItemOptions
+  ): this {
+    return this.add(
+      new ReceiptPropertiesItem(
+        lines instanceof Function ? lines(this.support) : lines,
+        options
+      )
+    );
   }
 
   public toString(): string {
@@ -72,10 +139,6 @@ export class ReceiptTextItem extends ReceiptItem<ReceiptTextItemOptions> {
   }
 
   public format(receipt: Receipt): string {
-    const padToken: string = " ";
-    const maximumLineLength: number =
-      receipt.options.width - this.options.horizontalPadding * 2;
-
     if (this.options.horizontalPadding * 2 + 2 > receipt.options.width) {
       throw new ReceiptFormattingException(
         receipt,
@@ -90,7 +153,10 @@ export class ReceiptTextItem extends ReceiptItem<ReceiptTextItemOptions> {
     let line: string = "";
 
     for (const word of words) {
-      if (line.length + word.length + 1 > maximumLineLength) {
+      if (
+        line.length + word.length + 1 >
+        receipt.options.width - this.options.horizontalPadding * 2
+      ) {
         lines.push(line.trim());
         line = "";
       }
@@ -101,22 +167,13 @@ export class ReceiptTextItem extends ReceiptItem<ReceiptTextItemOptions> {
     lines.push(line.trim());
 
     return lines
-      .map((line: string) => {
-        while (line.length < maximumLineLength) {
-          if (this.options.align === "left") {
-            line = line + padToken;
-          } else if (this.options.align === "right") {
-            line = padToken + line;
-          } else {
-            line = padToken + line + padToken;
-          }
-        }
-
-        return line
-          .substr(0, maximumLineLength)
-          .padStart(receipt.options.width - this.options.horizontalPadding)
-          .padEnd(receipt.options.width);
-      })
+      .map((line: string) =>
+        receipt.support.toAlignedString(
+          line,
+          this.options.align,
+          this.options.horizontalPadding
+        )
+      )
       .join(EOL);
   }
 }
@@ -144,8 +201,43 @@ export class ReceiptBreakItem extends ReceiptItem<void> {
     return "".padStart(receipt.options.width);
   }
 
-  public getDefaultOptions(): void {
-    //
+  public getDefaultOptions(): void {}
+}
+
+export class ReceiptPropertiesItem extends ReceiptItem<
+  ReceiptPropertiesItemOptions
+> {
+  constructor(
+    public readonly lines: readonly ReceiptPropertiesLine[],
+    options?: ReceiptPropertiesItemOptions
+  ) {
+    super(options);
+  }
+
+  public format(receipt: Receipt): string {
+    const longestLabelWidth: number =
+      Math.max(
+        ...this.lines.map((line: ReceiptPropertiesLine) => line.label.length)
+      ) + this.options.separator.length;
+
+    return this.lines
+      .map((line: ReceiptPropertiesLine) =>
+        this.options.layout === "align"
+          ? (line.label + this.options.separator).padEnd(longestLabelWidth) +
+            " " +
+            line.value
+          : (line.label + this.options.separator).padEnd(
+              receipt.options.width - line.value.length
+            ) + line.value
+      )
+      .join(EOL);
+  }
+
+  public getDefaultOptions(): ReceiptPropertiesItemOptions {
+    return {
+      layout: "align",
+      separator: ":"
+    };
   }
 }
 
--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,31 @@
+import { ReceiptSupport } from ".";
+
 export type ReceiptOptions = {
+  readonly locale: string;
   readonly currency: string;
   readonly width: number;
 };
 
-export type ReceiptTextItemAlign = "left" | "right" | "center";
+export type ReceiptItemDataProvider<T> = (support: ReceiptSupport) => T;
+
+export type Align = "left" | "right" | "center";
+export type Layout = "align" | "spread" | "hug";
 
 export type ReceiptTextItemOptions = {
-  readonly align: ReceiptTextItemAlign;
+  readonly align: Align;
   readonly horizontalPadding: number;
 };
 
 export type ReceiptRulerItemOptions = {
   readonly pattern: string;
+};
+
+export type ReceiptPropertiesLine = {
+  readonly label: string;
+  readonly value: string;
+};
+
+export type ReceiptPropertiesItemOptions = {
+  readonly layout: Layout;
+  readonly separator: string;
 };
